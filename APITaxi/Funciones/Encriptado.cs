47ba088a21ddb51a2c0005d1c408d475b0adf1bf
Santiago Cuervo <santic9999@gmail.com>
2025-04-05 23:48:23 -0500

    Instalación de BCrypt

---
+++ APITaxi/Funciones/Encriptado.cs
@@ -0,0 +1,19 @@
+﻿using BCrypt.Net;
+
+namespace APITaxiV2.Funciones
+{
+    public static class Encriptado
+    {
+        private const int WorkFactor = 12;
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, WorkFactor);
+        }
+
+        public static bool VerifyPassword(string inputPassword, string storedHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(inputPassword, storedHash);
+        }
+    }
+}
